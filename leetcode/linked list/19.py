75d176863c740709751c9ff516997b191f39e677
DestroyPeace <Destroypeaces@gmail.com>
2022-12-10 19:17:40 +0000

    Did some practice - mostly didn't understand it due to it not being visual or debuggable without having a data structure that allows you to visualise however did some problewms.

---
+++ leetcode/linked list/19.py
@@ -0,0 +1,22 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        dummy = ListNode(0, head)
+
+        l, r = dummy, head
+
+        while n > 0 :
+            right = right.next
+            n -= 1
+
+        # ITERATING BOTH
+        while r:            
+            l, r = l.next, r.next
+
+        l.next = l.next.next
+        return dummy.next
+        # NOW I HAVE LOCATED THE NTH TERM - REMOVE IT
---
+++ leetcode/linked list/206.py
@@ -0,0 +1,25 @@
+"""
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+"""
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev, curr = None, head
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+            
+        return prev
+
+        
+        
+
+        
