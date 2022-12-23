ca296f48924700c0c56e2f8d2f05afe9fd627bb3
DestroyPeace <Destroypeaces@gmail.com>
2022-12-23 13:52:37 +0000

    Implemented the backtracking solution which involves visualising a tree and then working off of that tree. The tree is a BST and splitting the problem as such makes it able to be viewed as a recursive DFS question.

---
+++ leetcode/backtracking/78.py
@@ -0,0 +1,44 @@
+class Solution:
+    def subsets(self, nums) -> int:
+        """from itertools import combinations
+        
+        temp_res = []
+        final_res = []
+
+        for l in range(len(nums) + 1):
+            temp_res.extend(combinations(nums, l))
+        
+
+
+        for value in temp_res:
+            temp = []
+
+            for num in value:
+                temp.append(num)
+
+            final_res.append(temp)
+
+        return final_res
+        """
+
+        res = []
+        sub = []
+
+        def dfs(i):
+            if i >= len(nums):
+                # Appending onto the result which will be returned.
+                res.append(sub.copy())
+                return 
+            
+            sub.append(nums[i])
+            dfs(i + 1)
+
+            sub.pop()
+            dfs(i + 1)
+
+        dfs(0)
+        return res
+
+test = Solution()
+print(test.subsets(nums = [1, 2, 3]))
+
