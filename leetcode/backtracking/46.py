4ee64898a59f1c8934113024a952870291a9f376
DestroyPeace <Destroypeaces@gmail.com>
2022-12-24 12:53:42 +0000

    Used itertools to create another shortcut - will commit a backtracking one later.

---
+++ leetcode/backtracking/46.py
@@ -0,0 +1,23 @@
+class Solution:
+    def permute(self, nums):
+        from itertools import permutations
+        """
+        # Getting a list[tuple(int...)] and having to convert the variable into
+        # list[list[int]]
+        res = list(permutations(nums, len(nums)))
+        final_res = []
+
+        for value in res:
+            temp_res = []
+
+            for number in value:
+                temp_res.append(number)
+
+            final_res.append(temp_res)
+"""
+        return permutations(nums)
+
+test = Solution()
+print(test.permute([1,2,3]))
+    
+
