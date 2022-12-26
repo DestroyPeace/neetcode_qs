7b433ccbabbe74381e03d57db13caf69f923991d
DestroyPeace <Destroypeaces@gmail.com>
2022-12-26 13:24:27 +0000

    fibonacci - remembered the previous one however might have a go at writing a memoization dfs solution?

---
+++ leetcode/1-d programming/70.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a, b = 0, 1
+
+        for _ in range(n):
+            a, b = b, a + b
+
+        return b
+
+test = Solution()
+print(test.climbStairs(3))
