9ac1af9ef35fbce558bde6d16c1f0ad425aed6d2
DestroyPeace <Destroypeaces@gmail.com>
2022-12-27 13:35:41 +0000

    VE - Found the optimisation. By removing the checking of the entirety of the reversed list, only checking the new members of that reversed splice (the new l and r indexxes of the string), I saved time and got back up to around 80%.

--- leetcode/1-d programming/647.py
+++ leetcode/1-d programming/647.py
@@ -1,13 +1,11 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         res = 0
-        res_list = []
 
         for i in range(len(s)):
             l, r = i, i
             # Checking if a palindrome exists and that it's within bounds.
-            while l >= 0 and r < len(s) and s[l:r + 1] == "".join(reversed(s[l:r + 1])):
-                res_list.append(s[l:r + 1])
+            while l >= 0 and r < len(s) and s[l] == s[r]:
                 res += 1 # if len(s[l:r + 1]) == 1 else 2 # Adding 2 for each palindrome 
 
                 l -= 1
@@ -17,14 +15,12 @@ class Solution:
             l, r = i, i + 1
 
             # Checking if a palindrome exists and that it's within bounds.
-            while l >= 0 and r < len(s) and s[l:r + 1] == "".join(reversed(s[l:r + 1])):
-                res_list.append(s[l:r + 1])
+            while l >= 0 and r < len(s) and s[l] == s[r]:
                 res += 1 # if len(s[l:r + 1]) == 1 else 2 # Adding 2 for each palindrome 
 
                 l -= 1
                 r += 1
 
-        print(res_list)
         return res
 
 print(Solution().countSubstrings("aaa"))
