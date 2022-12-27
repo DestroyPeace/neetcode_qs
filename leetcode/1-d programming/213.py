18764d7a9aebd6c4f5a876ab9b36f85d6727c074
DestroyPeace <Destroypeaces@gmail.com>
2022-12-27 12:58:37 +0000

    made the solution by myself after watching the video explanation of how to do it, however, understand base cases and include them more often especially when you're not given the test case results.

---
+++ leetcode/1-d programming/213.py
@@ -0,0 +1,22 @@
+class Solution:
+    def rob(self, nums: int) -> int:
+        def house_rob(houses):
+            rob_1, rob_2 = 0, 0
+
+            for house in houses:
+
+                # Calculating maximum possible number in a 3 wide window. (accounting 
+                # for any previous mistakes by calculating the highest value now.)
+                temp = max(rob_1 + house, rob_2)
+
+                # Iterating now.
+                rob_1 = rob_2 
+                rob_2 = temp
+
+            return rob_2 
+
+        return max(house_rob(nums[:-1]), house_rob(nums[1:]))
+
+
+test = Solution()
+print(test.rob([2, 3, 2]))
