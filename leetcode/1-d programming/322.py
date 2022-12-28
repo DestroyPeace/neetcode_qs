36f4f68adc382e054f6042c76f38884f4c7107c5
DestroyPeace <Destroypeaces@gmail.com>
2022-12-28 13:27:46 +0000

    VS - Recurrence relation and this is also backtracking in a sense. Close to my original idea.

--- leetcode/1-d programming/322.py
+++ leetcode/1-d programming/322.py
@@ -1,5 +1,53 @@
 class Solution:
     def coinChange(self, coins, amount):
+        # Recurrence relation
+        # Default values memoization
+        dp = [float("inf")] * (amount + 1)
+
+        dp[0] = 0 
+
+        for a in range(1, amount + 1):
+            for coin in coins:
+                if a - coin >= 0:
+                    # Adding the extra coin we need for (1 + dp[a - coins])
+                    dp[a] = min(dp[a], 1 + dp[a - coin])
+
+        return dp[amount] if dp[amount] < float("inf") else -1
+        
+
+        """# BACKTRACKING MEMO ALGO Taken from comment section - 
+
+        memo = {}
+        def dfs(amount):
+            # If we have found our solution, return 0
+            if amount == 0:
+                return 0
+
+            # If we haven't found our amount, return highest.
+            if amount < 0:
+                return float("inf")
+
+            # Using memoization
+            if amount in memo:
+
+                # Prevents continuation of code.
+                return memo[amount]
+            
+            # Iterating through every possible value
+            memo[amount] = min(1 + dfs(amount - c) for c in coins )
+            return memo[amount]
+        
+        mini = dfs(amount)
+        return mini if mini < float("inf") else -1
+
+"""
+
+        """
+        MY SOLUTION - HAS ISSUES 
+
+        1) Returns wrong values due to implementing memoization.
+        2) When it does return the right values it's too slow. 
+
         dp = {}
 
         if amount == 0:
@@ -17,7 +65,6 @@ class Solution:
             if val not in dp:
                 dp[val] = times 
 
-
             for num in coins:
                 if val + num in dp:
                     res.append(min(times + 1, dp[val + num]))
@@ -27,7 +74,7 @@ class Solution:
         for num in coins:
             dfs(num, 1)
 
-        return min(res) if res else -1
+        return min(res) if res else -1"""
 
 test = Solution()
 print(test.coinChange([1, 2, 5], 11))
