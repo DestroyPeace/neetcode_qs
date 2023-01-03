720db677371486d76a9d8940f81faf819eed4be5
DestroyPeace <Destroypeaces@gmail.com>
2023-01-03 13:31:52 +0000

    VS - Essentially, it's just pascal's triangle whereby the sum of the paths is equal to the sum of the different ways to get there. Use two iterators, one for each row and one for each col. Iterate from bottom row to the top row, creating a new row for each of them and then add the values, changing the value of original row to the newRow??

---
+++ leetcode/2-d programming/62.py
@@ -0,0 +1,47 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        tempRow = [1] * n
+        for row in  range(1, m):
+            for col in range(1, n):
+                print(tempRow)
+                # we are using the current col value before reassigning it
+                tempRow[col] = tempRow[col - 1] + tempRow[col]
+                
+        return tempRow[-1]
+
+
+        """global res
+        target = (m - 1, n - 1)
+        res = 0
+
+        cache =  []
+
+
+        def dfs(row, col, prev_coord):
+            global res
+
+            if (row, col) in cache:
+                res += 1
+                return 
+
+            if row not in range(m) or col not in range(n):
+                return 
+
+            if (row, col) == target:
+                # Long start-up however, might save time later on.
+                for row, col in prev_coord:
+                    if (row, col) not in cache:
+                        cache.append((row, col))
+
+                res += 1
+                return
+
+            prev_coord.append((row, col))
+
+            dfs(row + 1, col, prev_coord.copy())
+            dfs(row, col + 1, prev_coord.copy())
+
+        dfs(0, 0, [])
+        return res """
+
+print(Solution().uniquePaths(23, 12))
