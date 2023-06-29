b22240937bf64680f51c942cecab9ecebd50152b
DestroyPeace <Destroypeaces@gmail.com>
2023-06-29 20:10:54 +0100

    VS - Don't want to ever talk about it again

--- leetcode/2-d programming/518.py
+++ leetcode/2-d programming/518.py
@@ -1,19 +1,20 @@
 class Solution:
     def change(self, amount: int, coins: list[int]) -> int:
-        
-        # Creating the array with the amount as the y value, and the coins as the x value
         dp = [[0] * (len(coins) + 1) for i in range(amount + 1)]
-
-        # Seting the first value to 0 as 0 of each coin will always give a value of 0.
         dp[0] = [1] * (len(coins) + 1)
 
         for a in range(1, amount + 1):
             for i in range(len(coins) - 1, -1, -1):
-               dp[a][i] = dp[a][i + 1]
+                dp[a][i] = dp[a][i + 1]
 
+                if a - coins[i] >= 0:
+                    dp[a][i] += dp[a - coins[i]][i]
+        
+        return dp[amount][0]
+        
 
+        
     
-print(Solution().change(6, [1, 2, 3]))
+test = Solution()
 
-
-
+print(test.change(6, [1, 2, 3]))
