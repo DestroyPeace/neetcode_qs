0dc9c7b2a32b48682d44a581b01418717997b93e
DestroyPeace <Destroypeaces@gmail.com>
2023-07-03 16:34:12 +0100

    VS / NV - Created a working DFS solution, however did not implement a cache. The Cache was unable to be implemented due to the approach of multiple starting calls and disregard for where the return statement for the cache was. Also, did not include the end base case for the checking of index sums.

--- leetcode/2-d programming/494.py
+++ leetcode/2-d programming/494.py
@@ -1,25 +1,86 @@
 class Solution:
     def findTargetSumWays(self, nums: list[int], target: int) -> int:
+        cache = {
+
+        }
+
+        def dfs(index, total):
+
+            # Checking if the entire array has been passed
+            if index == len(nums):
+                if total == target:
+                    return 1
+                else:
+                    return 0
+                
+            # Checking if it's in cache
+            if (index, total) in cache:
+                return cache[(index, total)]
+
+            cache[(index, total)] = (dfs(index + 1, total - nums[index])) + (dfs(index + 1 , total + nums[index]))
+
+            return cache[(index, total)]
+
+        return dfs(0, 0)
+
+
+        """
+        DFS - Own solution
+
         res = []
         
         def dfs(index, total):
-            if index == len(nums):
+            # Checking if the entire array has been passed
+            if index == len(nums) - 1:
+
                 if total == target:
                     res.append("1")
                     return
                 else:
                     return 
-            
 
+            # Calculate if coin is taken (equivalent to a -)
             dfs(index + 1, total - nums[index + 1])
+            # Calculate if coin is not taken (equivalent to a +)
             dfs(index + 1, total + nums[index + 1])
 
     
-        dfs(0, target - nums[0])
-        dfs(0, target + nums[0])
-        return len(res)
+        # Calculate the first path where the value is subtracted
+        dfs(0, 0 - nums[0])
+
+        # Calculate the second path where the coin is added
+        dfs(0, 0 + nums[0])
+        return len(res)"""
+
+
+        """
+        DEBUG CODE
+        
+        def dfs(index, total, steps):
+            # Checking if the entire array has been passed
+            if index == len(nums) - 1:
+                print(total, target, steps)
+
+                if total == target:
+                    res.append("1")
+                    return
+                else:
+                    return 
+
+            # Calculate if coin is taken (equivalent to a -)
+            dfs(index + 1, total - nums[index + 1], steps[::] + ["-", total - nums[index + 1]])
+
+            # Calculate if coin is not taken (equivalent to a +)
+            dfs(index + 1, total + nums[index + 1], steps[::] + ["+", total + nums[index + 1]])
+
+    
+        # Calculate the first path where the value is subtracted
+        dfs(0, 0 - nums[0], ["-", 0 - nums[0]])
+
+        # Calculate the second path where the coin is added
+        dfs(0, 0 + nums[0], ["+", 0 + nums[0]])
+        return len(res)"""
 
 test = Solution()
-
 print(test.findTargetSumWays(nums = [1, 1, 1, 1, 1], target = 3))
             
