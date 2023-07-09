c2f97403c625dab304b00c56e4bcd34ce47f96fb
DestroyPeace <Destroypeaces@gmail.com>
2023-07-09 20:25:40 +0100

    VS - The way their method worked was backtracking using a two dimensional array where default value was set to false. They would utilise the idea that S3 index was a sum of the two indexxes of S1 and S2. Proceed with this ideology, referencing the previous elements to ensure that they were True before continuing in the same direction. Utilised the same idea in the previous method with the checking for values using indices.

--- leetcode/2-d programming/97.py
+++ leetcode/2-d programming/97.py
@@ -2,7 +2,7 @@
 
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        """if len(s1) + len(s2) != len(s3):
+        if len(s1) + len(s2) != len(s3):
             return False
 
         dp = [[False] * (len(s2) + 1) for i in range(len(s1) + 1)]
@@ -14,27 +14,7 @@ class Solution:
                     dp[i][j] = True
                 if j < len(s2) and s2[j] == s3[i + j] and dp[i][j + 1]:
                     dp[i][j] = True
-        return dp[0][0]"""
-
-        dp = {}
-
-        def dfs(i, j):
-            # Out of bounds case
-            if i == len(s1) and j == len(s2):
-                return True 
-
-            if (i, j) in dp:
-                return dp[(i, j)]
-            
-            if i < len(s1) and s1[i] == s3[i + j] and dfs(i + 1, j):
-                return True 
-            if j < len(s2) and s2[j] == s3[i + j] and dfs(i, j + 1):
-                return True 
-            dp[(i, j)] = False 
-            return False 
-        
-        dfs(0, 0)
-            
+        return dp[0][0]
 
 
         """if len(s3) != (len(s1) + len(s2)):
