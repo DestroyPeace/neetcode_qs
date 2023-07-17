6a9b8c36e6e7333f135ca5725c7d0e5dda42d0cc
DestroyPeace <Destroypeaces@gmail.com>
2023-07-17 14:40:03 +0100

    VS - Initially tried the problem - had no clue. Essentially, it's decomposition into smaller problems by iterating through each of the potential starting positions. Finish next time.

---
+++ leetcode/2-d programming/312.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        nums = [1] + nums + [1]
+
+        cache = {
+
+        }
+
+        @cache
+        def dfs(l, r):  
+            # Base Cases
+
+            if l > r:
+                return 0
+            
+            if (l, r) in cache:
+                return cache[(l, r)]
+            
+            cache[(l, r)] = 0
+
+            # Assuming each indexxed pair is popped last
+            for i in range(l, r + 1):
+                coins = nums[l-1] * nums[i] * nums[r + 1] 
+
+                coins += dfs(l, i - 1) + dfs(i + 1, r)
+                
+
+
+
+
+        return dfs(1, len(nums) - 2)
