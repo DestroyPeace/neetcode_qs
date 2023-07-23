f8dcc6a325cae4d25aff321f5b4fa6ba20d160ba
DestroyPeace <Destroypeaces@gmail.com>
2023-07-23 17:47:16 +0100

    VS - Essentially, treat each of the indices as the last point and work backwards, popping each of the values by shifting the window of operation smaller and smaller and then caching the maximum values of those in the array, and then also iterating through each of the possible values. The reason it's O(n^3) is due to the possibility of each neighbour including the original value having to be looped through, causing three loops.

--- leetcode/2-d programming/312.py
+++ leetcode/2-d programming/312.py
@@ -1,5 +1,5 @@
 class Solution:
-    def maxCoins(self, nums: List[int]) -> int:
+    def maxCoins(self, nums: list[int]) -> int:
         nums = [1] + nums + [1]
 
         cache = {
@@ -23,8 +23,10 @@ class Solution:
                 coins = nums[l-1] * nums[i] * nums[r + 1] 
 
                 coins += dfs(l, i - 1) + dfs(i + 1, r)
+                cache[(l, r)] = max(cache[(l, r)], coins)
                 
 
+            return cache[(l, r)]
 
 
 
