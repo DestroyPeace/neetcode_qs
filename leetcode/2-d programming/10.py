1e9e6fd7baebe36244bd74b685d5771eb5de019e
DestroyPeace <Destroypeaces@gmail.com>
2023-07-23 18:52:25 +0100

    VS - Implemented caching which saves time by reducing the callbacks and returning known statements. It also allows quicker denials of paths in a visual way. Storing each of the results is imperative to saving time.

--- leetcode/2-d programming/10.py
+++ leetcode/2-d programming/10.py
@@ -3,7 +3,12 @@ class Solution:
         if p == ".*" and s:
             return True 
         
+        cache = {}
+
         def dfs(i, j):
+            if (i, j) in cache:
+                return cache[(i, j)]
+
             if i >= len(s) and j >= len(p):
                 return True 
             
@@ -13,13 +18,17 @@ class Solution:
             match_case = i in range(len(s)) and (s[i] == p[j] or p[j] == ".")
 
             if (j + 1) <= len(p) and p[j + 1] == "*":
-                return (dfs(i, j + 2) or   # USING THE STAR
+                cache[(i, j)] = (dfs(i, j + 2) or   # USING THE STAR
                     match_case and dfs(i + 1, j)) # CHECKING FI THE LETTERS MATCH AND USING THE STAR
 
+                return cache[(i, j)]
+        
             # If no star charaacter
 
             if match_case:
-                return dfs(i + 1, j + 1)
+                cache[(i, j)] = dfs(i + 1, j + 1)
+
+                return cache[(i, j)]
             
             return False 
 
