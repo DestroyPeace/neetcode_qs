880e14525716eb5b8aa7163c4c551ab33635670f
DestroyPeace <Destroypeaces@gmail.com>
2023-08-06 15:44:05 +0100

    Fixed the problem with the numbers - used binary numbers instead of denary causing extra 1's to appear.

--- leetcode/bit manipulation/338.py
+++ leetcode/bit manipulation/338.py
@@ -1,16 +1,37 @@
 class Solution:
     def countBits(self, n: int) -> list[int]:
-        dp = [0] * (n + 1)
+        res_array = []
+
+        def num_of_1_bits(den_num):
+            res = 0
+
+            while den_num:
+                res += den_num % 2
+
+                den_num >>= 1
+
+            return res 
+        
+
+        for num in range(0, n + 1):
+            res_array.append(num_of_1_bits(num))
+
+        return res_array
+
+
+
+        """dp = [0] * (n + 1)
         ans = [0]
 
         offset = 1
-
         for i in range(1, n + 1):
             
             # Ensure each time you double the value of the offset.
             if offset * 2 == i:
                 offset = i
 
-            dp[i] = 1 + dp[i - offset]
+            dp[i] = 1 + dp[i - offset
+        return dp"""
 
-        return dp
+test = Solution()
+print(test.countBits(7))
