063baca9be6f5247f88b7da611eb9e41c6b04424
DestroyPeace <Destroypeaces@gmail.com>
2023-08-06 15:35:43 +0100

    I have no clue why this doesn't work in counting the number of bits?

--- leetcode/bit manipulation/338.py
+++ leetcode/bit manipulation/338.py
@@ -5,26 +5,36 @@ class Solution:
 
         def num_of_1_bits(den_num):
             num = int(bin(den_num)[2:])
-            print(num)
 
             res = 0
 
-            print("\nnew\n")
             while num:
                 res += num % 2 
-                print(f"{num} before")
-                print("=======================")
+
+                print(bin(num), den_num)
                 num >>= 1
-                print(f"{num} after")
 
             return res 
 
-        for number in range(1, n + 1):
+        for number in range(0, n):
             final_arr.append(num_of_1_bits(number))
 
         return final_arr
 
-print(Solution().countBits(12))
-            
 
+# print(Solution().countBits(5))
 
+def num_of_1_bits(den_num):
+            num = int(bin(den_num)[2:])
+
+            res = 0
+
+            while num:
+                print(num, den_num, num % 2)
+                res += num % 2 
+
+                num >>= 1
+
+            return res 
+
+print(num_of_1_bits(2))
