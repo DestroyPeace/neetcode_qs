7bb55cf772df4b7bf7c3e04913ed95f42e0674fe
DestroyPeace <Destroypeaces@gmail.com>
2023-08-06 15:41:24 +0100

    VS - Wasn't able to even figure out it was a dp problem - next time look for patterns?

--- leetcode/bit manipulation/338.py
+++ leetcode/bit manipulation/338.py
@@ -1,40 +1,16 @@
 class Solution:
     def countBits(self, n: int) -> list[int]:
-        final_arr = []
+        dp = [0] * (n + 1)
+        ans = [0]
 
+        offset = 1
 
-        def num_of_1_bits(den_num):
-            num = int(bin(den_num)[2:])
+        for i in range(1, n + 1):
+            
+            # Ensure each time you double the value of the offset.
+            if offset * 2 == i:
+                offset = i
 
-            res = 0
+            dp[i] = 1 + dp[i - offset]
 
-            while num:
-                res += num % 2 
-
-                print(bin(num), den_num)
-                num >>= 1
-
-            return res 
-
-        for number in range(0, n):
-            final_arr.append(num_of_1_bits(number))
-
-        return final_arr
-
-
-# print(Solution().countBits(5))
-
-def num_of_1_bits(den_num):
-            num = int(bin(den_num)[2:])
-
-            res = 0
-
-            while num:
-                print(num, den_num, num % 2)
-                res += num % 2 
-
-                num >>= 1
-
-            return res 
-
-print(num_of_1_bits(2))
+        return dp
