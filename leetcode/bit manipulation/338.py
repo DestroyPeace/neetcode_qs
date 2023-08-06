94ef0a78debf89df448877d2cc292aa871b7ab18
DestroyPeace <Destroypeaces@gmail.com>
2023-08-06 15:45:49 +0100

    Added comments on  the speed of each one. The first solution is much slower due to a lack of dp caching / memoization.

--- leetcode/bit manipulation/338.py
+++ leetcode/bit manipulation/338.py
@@ -2,6 +2,13 @@ class Solution:
     def countBits(self, n: int) -> list[int]:
         res_array = []
 
+
+
+        """
+        SLOW METHOD - FIRST METHOD
+
+        BEAT 28% OF PEOPLE.
+        
         def num_of_1_bits(den_num):
             res = 0
 
@@ -17,10 +24,13 @@ class Solution:
             res_array.append(num_of_1_bits(num))
 
         return res_array
+"""
 
 
-
-        """dp = [0] * (n + 1)
+        """
+        FAST DP METHOD - BEAT 99% OF PEOPLE
+        
+        dp = [0] * (n + 1)
         ans = [0]
 
         offset = 1
