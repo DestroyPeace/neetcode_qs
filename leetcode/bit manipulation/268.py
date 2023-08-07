cece9394cb1aa486ad7432febf14ef3600cc75d9
DestroyPeace <Destroypeaces@gmail.com>
2023-08-07 19:14:52 +0100

    VS - Essentially, you iterate through the range rather than creating a new list which reduces the memory complexity to O(1), and is also faster. This works by essentially using the commutative property of the addition to weed out the missing number.

--- leetcode/bit manipulation/268.py
+++ leetcode/bit manipulation/268.py
@@ -1,7 +1,29 @@
 class Solution:
     def missingNumber(self, nums: list[int]) -> int:
+
+        """
+        XOR METHOD
+
+        res = 0
+
+        for i in range(len(nums)):
+            res += (i - nums[i])
+
+        return res
+        
+        """
+
+        """
+        SUM METHOD        
+        
+        
+        """
+
+        """
+        SET INTERFERENCE TECHNIQUE
+
         res = set(range(len(nums) + 1))
 
         return (res - set(nums)).pop()
-    
+    """
 print(Solution().missingNumber([3, 0 , 1]))
