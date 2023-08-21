23cc2fa6961a8acacdfbcdea0d110c1cd3323fe0
DestroyPeace <Destroypeaces@gmail.com>
2023-08-21 16:41:23 +0100

    VE / VS - Had the right initial idea of using two heaps, however essentially just remember to use two heaps to separate the array and to then find the median between those two heaps by either checking their differing lengths or checking for the middle of the two through the O(1) time lookups for the max for the max_heap (small_heap) and the O(1) time for the lookup of the min_heap (large_heap).

--- leetcode/heaps/295.py
+++ leetcode/heaps/295.py
@@ -3,24 +3,43 @@ import heapq
 class MedianFinder:
 
     def __init__(self):
-        self.min_heap = []
+        self.small_heap = []
+        self.large_heap = []
 
-        heapq.heapify(self.min_heap)
+        # Creating two heaps to find the middle of the max_heap small_heap and the min_heap large_heap.
+        heapq.heapify(self.small_heap)
+        heapq.heapify(self.large_heap)
         
 
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.min_heap, num)
         
-        
+        if self.large_heap and num > self.large_heap[0]:
+            heapq.heappush(self.large_heap, num)
+        else:
+            heapq.heappush(self.small_heap, -1 * num)
+
+        # Checking that the length of both lists is within 1 to ensure that it is possible to calculate
+        # even / odd. 
+
+        if len(self.small_heap) > len(self.large_heap) + 1:
+            res = heapq.heappop(self.small_heap) * -1 # MAX_HEAP[0] -> MIN_HEAP[0]
+            heapq.heappush(self.large_heap, res)
+
+        if len(self.large_heap) > len(self.small_heap) + 1:
+            res = heapq.heappop(self.large_heap) * -1
+            heapq.heappush(self.small_heap, res)   
 
     def findMedian(self) -> float:
-        length = len(self.min_heap)
 
-        # Even case
-        if length % 2 == 0:
-            return (self.min_heap[length // 2 - 1] + self.min_heap[length // 2]) / 2
-        else:
-            return self.min_heap[(length + 1) // 2 - 1]
+        if len(self.large_heap) > len(self.small_heap):
+            return self.large_heap[0]
+    
+        elif len(self.small_heap) > len(self.large_heap):
+            return self.small_heap[0] * -1
+
+        return ((self.small_heap[0] * -1) + self.large_heap[0]) / 2
+        
+
         
 
 
