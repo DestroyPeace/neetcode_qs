d2bc1cc163e73f2461106d12a87a0ab6600113d0
DestroyPeace <Destroypeaces@gmail.com>
2023-09-18 20:00:30 +0100

    VS - Lost it when he said it should be a short solution. Essentially, rather than the O(n^2) solution I used whcih was to essentially to continue to iterate from each starting position, you are able to skip each of the negative values by ensuring that they are skipped out at the start as well as ensuring that you are able to just iterate through the entirety of the list as you are guaranteed to find a solution.

--- leetcode/greedy/134.py
+++ leetcode/greedy/134.py
@@ -10,34 +10,17 @@ class Solution:
         if gas == cost:
             return 0
 
-        diff = [gas[i] - cost[i] for i in range(len(gas))]
-        # Choosing the new starting index
-        for start in range(len(diff)):
-            total = diff[start]
+        total = 0
+        res = 0
 
-            # Choosing starting position
-            if diff[start] > 0:
-                pos = start
+        for i in range(len(gas)):
+            total += (gas[i] - cost[i])
 
-                for _ in range(len(diff) - 1):
-                    # Checking the next index
-                    if pos + 1 in range(len(diff)):
-                        pos += 1
+            if total < 0: 
+                total = 0
+                res = i  + 1
 
-                    # Means we've reached the end of the array.
-                    else:
-                        pos = 0
-                    
-                    total += diff[pos]
-
-                    if total < 0:
-                        break
-                
-                # CURR POS == START
-                if gas[pos] == gas[start - 1]:
-                    return start
-
-        return -1
+        return res
         
 test = Solution()
 print(test.canCompleteCircuit([3,1,1], [1,2,2]))
