4e2b01fe369718f1658b510094d1b5a35487807b
DestroyPeace <Destroypeaces@gmail.com>
2023-10-26 08:08:40 +0100

    Essentially, the way it works is due to how the recursive function is able to iterate through each of the possible values ranging from 1 to the final power, where the power is equal to the previous results being multiplied by each other and essentially it keeps squaring itself until it reaches the final value, and it is also multiplied by x if it's odd.

--- leetcode/maths and geometry/50.py
+++ leetcode/maths and geometry/50.py
@@ -1,3 +1,21 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return (x ** n)
+        if x == 0:
+            return 0
+
+        def helper(x, n):
+            if n == 0:
+                return 1 
+            
+
+            res = helper(x, n // 2)
+            res = res * res 
+
+            return x * res if n % 2 else res 
+
+        res = helper(x, abs(n))
+
+        return res if n >= 0 else 1 / res 
+
+test = Solution()
+test.myPow(2, 10)
