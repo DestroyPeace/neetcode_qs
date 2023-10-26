0a78d1545855b4a0ad99c03c386ccdf1f0c15826
DestroyPeace <Destroypeaces@gmail.com>
2023-10-26 08:51:42 +0100

    CS - Improved space complexity by removing the need for the extra list. This effectively reduced the memory cost from O(n^2) to O(n). This was done by utilising the point as the key for a defaultdict and then iterating over the keys of this defaultdict. Another change was how we had to also multiply by the occurences of the (x, y), although not too sure why yet.

--- leetcode/maths and geometry/2013.py
+++ leetcode/maths and geometry/2013.py
@@ -4,12 +4,10 @@ from collections import defaultdict
 
 class DetectSquares:
     def __init__(self):
-        self.ptsCount = defaultdict(int)
-        self.pts = []
+        self.pts = defaultdict(int)
 
     def add(self, point: List[int]) -> None:
-        self.ptsCount[tuple(point)] += 1
-        self.pts.append(point)
+        self.pts[tuple(point)] += 1
 
     def count(self, point: List[int]) -> int:
         res = 0
@@ -19,12 +17,14 @@ class DetectSquares:
             # SECOND and THIRD conditions are due to the square having to have a positive area.
             if (abs(py - y) != abs(px - x)) or x == px or y == py:
                 continue
-            res += self.ptsCount[(x, py)] * self.ptsCount[(px, y)]
+
+            if (px, y) in self.pts and (x, py) in self.pts:
+                res += self.pts[(x, py)] * self.pts[(px, y)] * self.pts[(x, y)]
+
         return res
 
 
         
-
 
 # Your DetectSquares object will be instantiated and called as such:
 # obj = DetectSquares()
