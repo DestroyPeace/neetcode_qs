836bd2b12c4d6ff1d5770e064a22357c7848a538
DestroyPeace <Destroypeaces@gmail.com>
2023-10-29 09:12:17 +0000

    VS - DFS recursive approach included alongside the optimal DP approach (Memory saved from N -> 1 going from DFS to DP). This approach is a lot more intuitive and essentially finds the max using memoisation.

--- leetcode/extra practice/1911.py
+++ leetcode/extra practice/1911.py
@@ -2,7 +2,7 @@ List = list
 
 class Solution:
     def maxAlternatingSum(self, nums: List[int]) -> int:
-        # Basically, these are assuming if the first number was added (sumEven), 
+        """# Basically, these are assuming if the first number was added (sumEven), 
         # or the first number was subtracted. 
 
         sumEven, sumOdd = 0, 0
@@ -14,4 +14,21 @@ class Solution:
             sumEven, sumOdd = tmpEven, tmpOdd
         
         # You always start off on even. 
-        return sumEven 
+        return sumEven """
+
+        dp = {}
+
+        def dfs(i, even):
+            if i == len(nums):
+                return 0
+            
+            if (i, even) in dp:
+                return dp[(i, even)]
+            
+            total = nums[i] if even else (-1 * nums[i])
+
+            dp[(i, even)] = max((total + dfs(i + 1, not even)), dfs(i + 1, even))
+
+            return dp[(i, even)]
+        
+        return dfs(0, True)
