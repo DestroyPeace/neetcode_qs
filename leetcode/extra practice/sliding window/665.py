f4b31453edc37f0797be63ab05ccf54f9d97acf2
DestroyPeace <Destroypeaces@gmail.com>
2023-11-26 17:55:06 +0000

    VS - The reason I shouldn't have used sliding window is due to the fact that the window is always size 1 meaning that I can just iterate through and add an increment of 1 to account for the sliding window aspect of it. I also needed to not just assume that I could take the max or the min or to only compare pairs, but to check all of them at the same time.

--- leetcode/extra practice/sliding window/665.py
+++ leetcode/extra practice/sliding window/665.py
@@ -2,27 +2,20 @@ List = list
 
 class Solution:
     def checkPossibility(self, nums: List[int]) -> bool:
-        if len(nums) <= 2: 
-            return True 
-
-        l, r = 0, 1
-        max_seen = float("-inf")
-        passed = False 
-
-        while r < len(nums): 
-            if (nums[l] > nums[r]) and passed:
-                return False 
-            elif (nums[l] > nums[r]):
-                passed = True
-
-                # Changing it to be 1 more than the max
-                nums[r] = max_seen + 1
-            
-            max_seen = max(nums[r], max_seen)
-            l += 1
-            r += 1
-    
-        return True 
+        if len(nums) <= 2:
+            return True
+        changed = False
+        for i, num in enumerate(nums):
+            if i == len(nums) - 1 or num <= nums[i + 1]:
+                continue
+            if changed:
+                return False
+            if i == 0 or nums[i + 1] >= nums[i - 1]:
+                nums[i] = nums[i + 1]
+            else:
+                nums[i + 1] = nums[i]
+            changed = True
+        return True
     
 test = Solution()
 print(test.checkPossibility(nums = [3,4,2,3]))
