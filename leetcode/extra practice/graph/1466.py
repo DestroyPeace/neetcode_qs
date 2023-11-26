ddbc6a76b15ec92be29327fc198b325d5cdcec66
DestroyPeace <Destroypeaces@gmail.com>
2023-11-26 18:26:31 +0000

    CS - Essentially the same process however you just use a queue and iterate through each of the neighbours that way, checking if they are reversed or not (working backwards from 0 essentially).

--- leetcode/extra practice/graph/1466.py
+++ leetcode/extra practice/graph/1466.py
@@ -6,8 +6,6 @@ List = list
 
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        global changes
-
         edges = {
             (a,b) for a, b in connections
         }
@@ -19,33 +17,28 @@ class Solution:
         visit = set()
         changes = 0
 
-        # Identifying all of the neighbours. 
         for a, b in connections:
             neighbours[a].append(b)
             neighbours[b].append(a)
 
-        def dfs(city): 
-            global changes
-            for neighbour in neighbours[city]: 
-                
-                # This neighbour has already been visited and this means that it already
-                # is already going to 0 and has already been accounted for.
-                if neighbour in visit:
-                    continue 
-                
-                # This means the graph is currently backwards, and knowing that 
-                # the current city is able to go to 0, we must be able to traverse backwards.
-                if (neighbour, city) not in edges: 
-                    changes += 1
+        q = collections.deque([0])
 
-                visit.add(neighbour) 
-                dfs(neighbour)
-        
         visit.add(0)
-        dfs(0)
 
+        while q:
+            city = q.popleft() 
+
+            for nei in neighbours[city]:
+                if nei in visit:
+                    continue 
+
+                if (city, nei) in edges: 
+                    changes += 1
+                
+                q.append(nei)
+                visit.add(nei)
+        
         return changes
-
         
 test = Solution()
 print(test.minReorder(n = 6, connections = [[0,1],[1,3],[2,3],[4,0],[4,5]]))
