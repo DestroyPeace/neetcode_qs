11e422c26dce4a4621d51cc7f2e40ef41558265e
DestroyPeace <Destroypeaces@gmail.com>
2024-01-05 19:09:18 +0000

    NV - Timeout error on the last question. Redundant loops increasing time complexity to around O(n^2) rather than the O(10n) solution. Use a defaultdict in future to just go over the array once in total.

--- leetcode/extra practice/187.py
+++ leetcode/extra practice/187.py
@@ -1,10 +1,52 @@
+"""
+The DNA sequence is composed of a series of nucleotides abbreviated as 'A', 'C', 'G', and 'T'.
+
+For example, "ACGAATTCCG" is a DNA sequence.
+When studying DNA, it is useful to identify repeated sequences within the DNA.
+
+Given a string s that represents a DNA sequence, return all the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule. You may return the answer in any order.
+"""
+
+import heapq
+import collections 
+import math
+
+List = list         
+
 class Solution:
-    def findRepeatedDnaSequences(self, s: str) -> list[str]:
-        result = set()
-        previous_sequences = set()
-        for i in range(len(s) - 9):
-            current = s[i:i+10]
-            if current in previous_sequences:
-                result.add(current)
-            previous_sequences.add(current)
-        return list(result)
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        # Identifying all the 10 letter sequences 
+        res = []
+
+        # Base case 
+        if len(s) < 10:
+            return 0 
+        
+        # Identifying all of the varying levels - we have to accept that it'll be a list 
+        # due to the fact that certain variations where it's just one letter will have multiple
+        # varying "checks" but each of them are differing in their index.
+
+        check = [
+            s[i: i + 10] for i in range(len(s) - 9)
+        ]
+        
+        for c in check:
+            temp = 0 
+            
+            for i in range(len(s) - 9): 
+                if s[i: i + 10] == c and c not in res:
+                    temp += 1
+
+            # Using the heuristic that splitting it will remove all of the copies therefore
+            # if the length is less than 10 less therefore we know that there must be at least 2.
+            if temp > 1: 
+                res.append(c) 
+
+        return res 
+        
+
+
+        
+test = Solution()
+print(test.findRepeatedDnaSequences(s = "AAAAAAAAAAA"))
+ 
